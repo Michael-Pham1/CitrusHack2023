3a8491643fdd42242c5ea0767de16293687d105c
Michael-Pham1 <47127433+Michael-Pham1@users.noreply.github.com>
2023-04-29 12:48:14 -0700

    Smoothed Character movement

--- Maze Runner/Assets/Scripts/CharacterMovement.cs
+++ Maze Runner/Assets/Scripts/CharacterMovement.cs
@@ -4,34 +4,27 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    private float xDir;
+    private float yDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow)){
-            Vector2 position = this.transform.position;
-            position.x--;
-            this.transform.position = position;
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow)){
-            Vector2 position = this.transform.position;
-            position.x++;
-            this.transform.position = position;
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow)){
-            Vector2 position = this.transform.position;
-            position.y++;
-            this.transform.position = position;
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow)){
-            Vector2 position = this.transform.position;
-            position.y--;
-            this.transform.position = position;
-        }
+        // if (Input.GetKeyDown(KeyCode.LeftArrow)){
+        //     Vector2 position = this.transform.position;
+        //     position.x--;
+        //     this.transform.position = position;
+        // }
+        xDir = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xDir * 7f, rb.velocity.y);
+        yDir = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(rb.velocity.x, yDir * 7f);
     }
 }
